c056e39b14bb157066ec17158b9d6205b79fd3bb
Tim Otten <totten@civicrm.org>
2016-11-09 23:04:46 -0800

    CRM_Mosaico_Page_Editor - Simplfying rendering lifecycle
    The normal Civi rendering pipeline is designed to embed within a CMS page
    and cooperate with the CMS on resource management.  However, the Mosaico
    page is different -- it occupies the entire UI. This requires a work-around
    to opt-out of the normal CMS/Civi services.
    
    The `snippet=2` technique comes close but had some oddities:
     * The markup contained multiple `<body>` tags
     * The markup requires a work-around to disable printing
     * The services of `CRM_Core_Resources` aren't reliable within `snippet=2`.
       (This probably depends on which CMS and/or which aspect you're using.
       In Drupal 7, `addVars()` doesn't work with `snippet=2`.)
    
    This commit takes a "back-to-basics" approach.  Since most CMS/Civi services
    don't actually make sense on this page, skip them.  Just output the HTML we
    want.

--- CRM/Mosaico/Page/Editor.php
+++ CRM/Mosaico/Page/Editor.php
@@ -4,35 +4,44 @@ class CRM_Mosaico_Page_Editor extends CRM_Core_Page {
   const DEFAULT_MODULE_WEIGHT = 200;
 
   function run() {
-    $this->registerResources(CRM_Core_Resources::singleton());
-    return parent::run();
+    $smarty = CRM_Core_Smarty::singleton();
+    $smarty->assign('scriptUrls', $this->getScriptUrls());
+    $smarty->assign('styleUrls', $this->getStyleUrls());
+    echo $smarty->fetch('CRM/Mosaico/Page/Editor.tpl');
+    CRM_Utils_System::civiExit();
   }
 
-  /**
-   * @param CRM_Core_Resources $res
-   */
-  public function registerResources(CRM_Core_Resources $res) {
-    $weight = self::DEFAULT_MODULE_WEIGHT;
-    $distUrl = CRM_Mosaico_Utils::getMosaicoDistUrl('relative');
-
-    $res->addScriptUrl("$distUrl/vendor/knockout.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery-ui.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.ui.touch-punch.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/load-image.all.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/canvas-to-blob.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.iframe-transport.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.fileupload.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.fileupload-process.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.fileupload-image.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/jquery.fileupload-validate.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/knockout-jqueryui.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/vendor/tinymce.min.js", $weight++, 'html-header', FALSE);
-    $res->addScriptUrl("$distUrl/mosaico.min.js?v=0.15", $weight++, 'html-header', FALSE);
-
-    $res->addScriptFile("uk.co.vedaconsulting.mosaico", "js/editor.js", $weight++, 'html-header', FALSE);
-    
-    $res->addStyleUrl("$distUrl/mosaico-material.min.css?v=0.10", $weight++, 'html-header', TRUE);
-    $res->addStyleUrl("$distUrl/vendor/notoregular/stylesheet.css", $weight++, 'html-header', TRUE);
+  protected function getScriptUrls() {
+    $cacheCode = CRM_Core_Resources::singleton()->getCacheCode();
+    $mosaicoDistUrl = CRM_Mosaico_Utils::getMosaicoDistUrl('relative');
+    $mosaicoExtUrl = CRM_Core_Resources::singleton()->getUrl('uk.co.vedaconsulting.mosaico');
+    return array(
+      "{$mosaicoDistUrl}/vendor/knockout.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery-ui.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.ui.touch-punch.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/load-image.all.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/canvas-to-blob.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.iframe-transport.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.fileupload.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.fileupload-process.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.fileupload-image.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/jquery.fileupload-validate.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/knockout-jqueryui.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/tinymce.min.js?r={$cacheCode}",
+      "{$mosaicoDistUrl}/mosaico.min.js?v=0.15?&={$cacheCode}",
+      "{$mosaicoExtUrl}/js/editor.js?r={$cacheCode}"
+    );
   }
+
+  protected function getStyleUrls() {
+    $cacheCode = CRM_Core_Resources::singleton()->getCacheCode();
+    $mosaicoDistUrl = CRM_Mosaico_Utils::getMosaicoDistUrl('relative');
+    // $mosaicoExtUrl = CRM_Core_Resources::singleton()->getUrl('uk.co.vedaconsulting.mosaico');
+    return array(
+      "{$mosaicoDistUrl}/mosaico-material.min.css?v=0.10&r={$cacheCode}",
+      "{$mosaicoDistUrl}/vendor/notoregular/stylesheet.css?r={$cacheCode}",
+    );
+  }
+
 }

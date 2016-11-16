dfcc1622640cc80e1a65e9afd06be1ce7d4e0458
Tim Otten <totten@civicrm.org>
2016-11-16 14:05:20 -0800

    crmMosaico - Generate skeletal Angular module

---
+++ ang/crmMosaico.ang.php
@@ -0,0 +1,24 @@
+<?php
+// This file declares an Angular module which can be autoloaded
+// in CiviCRM. See also:
+// http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
+
+return array (
+  'js' => 
+  array (
+    0 => 'ang/crmMosaico.js',
+    1 => 'ang/crmMosaico/*.js',
+    2 => 'ang/crmMosaico/*/*.js',
+  ),
+  'css' => 
+  array (
+    0 => 'ang/crmMosaico.css',
+  ),
+  'partials' => 
+  array (
+    0 => 'ang/crmMosaico',
+  ),
+  'settings' => 
+  array (
+  ),
+);

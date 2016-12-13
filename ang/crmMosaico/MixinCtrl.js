d55e24ef559fecb3f10302bc74ace8e71efc3db4
Tim Otten <totten@civicrm.org>
2016-12-12 21:44:44 -0800

    Implement the "Test" button within Mosaico UI

--- ang/crmMosaico/MixinCtrl.js
+++ ang/crmMosaico/MixinCtrl.js
@@ -11,12 +11,13 @@
     $scope.mosaicoCtrl = {
       templates: [],
       select: function(mailing, template) {
-        var topt = mailing.template_options = $scope.mailing.template_options || {};
+        var topt = mailing.template_options = mailing.template_options || {};
         var promise = crmMosaicoTemplates.getFull(template).then(function(tplCtnt){
           topt.mosaicoTemplate = template.id;
           topt.mosaicoMetadata = tplCtnt.metadata;
           topt.mosaicoContent = tplCtnt.content;
           mailing.body_html = tplCtnt.html;
+          // console.log('select', {isAr1: _.isArray(mailing.template_options), isAr2: _.isArray(topt), m: mailing, t: template});
           $scope.mosaicoCtrl.edit(mailing);
         });
         return crmStatus({start: ts('Loading...'), success: null}, promise);
@@ -96,6 +97,14 @@
     // See https://github.com/voidlabs/mosaico/wiki/Mosaico-Plugins
     // Generally: Implement the in-dialog "Save" and "Test" buttons.
     function mosaicoPlugin(ko, viewModel) {
+      function syncModel() {
+        $scope.mailing.body_html = viewModel.exportHTML();
+        $scope.mailing.template_options = $scope.mailing.template_options || {};
+        // Mosaico exports JSON. Keep their original encoding... or else the loader throws an error.
+        $scope.mailing.template_options.mosaicoMetadata = viewModel.exportMetadata();
+        $scope.mailing.template_options.mosaicoContent = viewModel.exportJSON();
+      }
+
       var saveCmd = {
         name: 'Save', // l10n happens in the template
         enabled: ko.observable(true)
@@ -103,11 +112,7 @@
       saveCmd.execute = function() {
         saveCmd.enabled(false);
         viewModel.metadata.changed = Date.now();
-        $scope.mailing.body_html = viewModel.exportHTML();
-        $scope.mailing.template_options = $scope.mailing.template_options || {};
-        // Mosaico exports JSON. Keep their original encoding... or else the loader throws an error.
-        $scope.mailing.template_options.mosaicoMetadata = viewModel.exportMetadata();
-        $scope.mailing.template_options.mosaicoContent = viewModel.exportJSON();
+        syncModel();
         saveCmd.enabled(true);
         dialogService.close('crmMosaicoEditorDialog');
         $scope.save();
@@ -119,9 +124,20 @@
         enabled: ko.observable(true)
       };
       testCmd.execute = function() {
-        saveCmd.enabled(false);
-        CRM.alert('TODO: Test');
-        saveCmd.enabled(true);
+        // testCmd.enabled(false);
+        // CRM.alert('TODO: Test');
+        // testCmd.enabled(true);
+        syncModel();
+
+        var model = {mailing: $scope.mailing, attachments: $scope.attachments};
+        var options = CRM.utils.adjustDialogDefaults(angular.extend(
+          {
+            autoOpen: false,
+            title: ts('Test Mailing')
+          },
+          options
+        ));
+        return dialogService.open('crmMosaicoPreviewDialog', '~/crmMosaico/PreviewDialogCtrl.html', model, options);
       };
       viewModel.test = testCmd;
     }
---
+++ ang/crmMosaico/PreviewDialogCtrl.js
@@ -0,0 +1,30 @@
+(function(angular, $, _) {
+
+  // Controller for previewing mail-content from Mosaico
+  // Scope members:
+  //   - [input] "model": Object
+  //     - "mailing": Object, CiviMail mailing
+  //     - "attachments": Object, CrmAttachment
+  angular.module('crmCxn').controller('CrmMosaicoPreviewDialogCtrl', function CrmMosaicoPreviewDialogCtrl($scope, crmMailingMgr, crmMailingPreviewMgr, crmBlocker, crmStatus) {
+    var ts = $scope.ts = CRM.ts(null);
+    var block = $scope.block = crmBlocker();
+
+    // @return Promise
+    $scope.previewMailing = function previewMailing(mailing, mode) {
+      return crmMailingPreviewMgr.preview(mailing, mode);
+    };
+
+    // @return Promise
+    $scope.sendTest = function sendTest(mailing, attachments, recipient) {
+      var savePromise = crmMailingMgr.save(mailing)
+        .then(function() {
+          return attachments.save();
+        });
+      return block(crmStatus({start: ts('Saving...'), success: ''}, savePromise)
+        .then(function() {
+          crmMailingPreviewMgr.sendTest(mailing, recipient);
+        }));
+    };
+  });
+
+})(angular, CRM.$, CRM._);

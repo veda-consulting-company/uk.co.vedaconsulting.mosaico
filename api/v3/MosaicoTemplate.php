979cc5a06d28a1e1151ac941b47973c1412a8ef9
Tim Otten <totten@civicrm.org>
2016-12-14 12:41:33 -0800

    MosaicoTemplate.clone API
    Each MosaicoTemplte could be fairly large.  Doing the clone on the
    server-side should reduce the amount of data going round-trip.
    
    Note: The API could be named "copy" or "clone" or "duplicate"; chose "clone"
    because there's already another "Mailing.clone" API.

--- api/v3/MosaicoTemplate.php
+++ api/v3/MosaicoTemplate.php
@@ -44,3 +44,44 @@ function civicrm_api3_mosaico_template_delete($params) {
 function civicrm_api3_mosaico_template_get($params) {
   return _civicrm_api3_basic_get('CRM_Mosaico_BAO_MosaicoTemplate', $params);
 }
+
+/**
+ * Adjust metadata for clone spec action.
+ *
+ * @param array $spec
+ */
+function _civicrm_api3_mosaico_template_clone_spec(&$spec) {
+  $mailingFields = CRM_Mosaico_DAO_MosaicoTemplate::fields();
+  $spec['id'] = $mailingFields['id'];
+  $spec['id']['api.required'] = 1;
+}
+
+/**
+ * Clone mailing.
+ *
+ * @param array $params
+ *
+ * @return array
+ * @throws \CiviCRM_API3_Exception
+ */
+function civicrm_api3_mosaico_template_clone($params) {
+  $BLACKLIST = array('id');
+
+  $newParams = CRM_Utils_Array::subset($params, array(
+    'debug',
+    'title',
+    'base',
+    'html',
+    'metadata',
+    'content',
+  ));
+
+  $get = civicrm_api3('MosaicoTemplate', 'getsingle', array('id' => $params['id']));
+  foreach ($get as $field => $value) {
+    if (!isset($newParams[$field]) && !in_array($field, $BLACKLIST)) {
+      $newParams[$field] = $value;
+    }
+  }
+
+  return civicrm_api3('MosaicoTemplate', 'create', $newParams);
+}

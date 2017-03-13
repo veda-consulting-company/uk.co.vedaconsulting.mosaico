68d978523e74e590c7f37a10c434faa7d41d9089
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2017-03-13 16:56:20 +0000

    joomla and wordpress base url fix. Joomla pulled from https://github.com/veda-consulting/uk.co.vedaconsulting.mosaico/pull/59/files. These doesn't need to be submitted to v2.x as 2.x already has the generic fix.

--- js/editor.js
+++ js/editor.js
@@ -6,7 +6,9 @@ $(function() {
   var cmsPath = window.location.href.substr(0, window.location.href.lastIndexOf('/'));
   var imageUrl = cmsPath;
   if (cmsPath.indexOf('wp-admin/admin.php') !== -1) {//Fix me CRM.url 
-    imageUrl = cmsPath.replace('wp-admin/admin.php','');//In order to stop image urls get admin in there which causes images not being displayed in email for wordpress
+    imageUrl = cmsPath.replace('wp-admin/admin.php','civicrm');//In order to stop image urls get admin in there which causes images not being displayed in email for wordpress
+  } else if (cmsPath.indexOf('administrator/') !== -1) { //Fix me CRM.url
+    imageUrl = cmsPath.replace('administrator/',''); //In order to stop image urls get admin in there which causes images not being displayed in email for joomla
   }
   var plugins;
   // A basic plugin that expose the "viewModel" object as a global variable.

225dbbdf098e737abcc37cae1096a03a3fc45370
Maged Adel <maged@compucorp.co.uk>
2017-04-03 12:28:52 +0200

    HW-266: Print label based on template state

--- ang/crmMosaico/TemplateItem.js
+++ ang/crmMosaico/TemplateItem.js
@@ -2,7 +2,8 @@
   // "crmMosaicoTemplateItem" displays an actionable thumbnail.
   // To specify action handlers, use `on-item-click`, `on-item-preview`, etal.
   // To conditionally enable/disable actions, use `check-item-click`, `check-item-preview`, etal.
-  // Example usage: <div crm-mosaico-template-item="{title: 1, subtitle: 2, thumbnail: 3}" on-item-click="alert('Click')" on-item-preview="alert('Preview')"></div>
+  // Example usage: <div crm-mosaico-template-item="{state: 'selected', title: 1, subtitle: 2, thumbnail: 3}" on-item-click="alert('Click')" on-item-preview="alert('Preview')"></div>
+  // Avaiable state options are (select, selected, configure, new)
   angular.module('crmMosaico').directive('crmMosaicoTemplateItem', function() {
     return {
       restrict: 'AE',
@@ -12,13 +13,26 @@
       },
       link: function($scope, $el, $attr) {
         var ts = $scope.ts = CRM.ts('mosaico');
-        $scope.$watch('crmMosaicoTemplateItem', function(newValue){
+
+        $scope.$watch('crmMosaicoTemplateItem', function(newValue) {
           $scope.myOptions = newValue;
+
+          // Template default action label based on current state
+          if (newValue['state']) {
+            var clickActionLabels = {
+              select: "Select",
+              selected: "Edit",
+              configure: "Edit",
+              new: "New"
+            };
+            var clickActionLabel = clickActionLabels[newValue['state']];
+            $scope.actionLabel = clickActionLabel;
+          }
         });
         $scope.hasAction = function hasAction(action) {
-          if (!$attr['on'+action]) return false;
-          if ($attr['check'+action]) {
-            if (!$scope.$parent.$eval($attr['check'+action])) {
+          if (!$attr['on' + action]) return false;
+          if ($attr['check' + action]) {
+            if (!$scope.$parent.$eval($attr['check' + action])) {
               return false;
             }
           }

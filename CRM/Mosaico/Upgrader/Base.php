ef6f8285322942dad90551e0b97ec8a719be5356
Tim Otten <totten@civicrm.org>
2017-11-14 15:23:23 -0800

    CRM/Mosaico/Upgrader/Base.php - civilint

--- CRM/Mosaico/Upgrader/Base.php
+++ CRM/Mosaico/Upgrader/Base.php
@@ -36,11 +36,11 @@ class CRM_Mosaico_Upgrader_Base {
    * Obtain a reference to the active upgrade handler.
    */
   static public function instance() {
-    if (! self::$instance) {
+    if (!self::$instance) {
       // FIXME auto-generate
       self::$instance = new CRM_Mosaico_Upgrader(
         'uk.co.vedaconsulting.mosaico',
-        realpath(__DIR__ .'/../../../')
+        realpath(__DIR__ . '/../../../')
       );
     }
     return self::$instance;
@@ -205,7 +205,7 @@ class CRM_Mosaico_Upgrader_Base {
    * @return array(revisionNumbers) sorted numerically
    */
   public function getRevisions() {
-    if (! is_array($this->revisions)) {
+    if (!is_array($this->revisions)) {
       $this->revisions = array();
 
       $clazz = new ReflectionClass(get_class($this));
@@ -240,6 +240,9 @@ class CRM_Mosaico_Upgrader_Base {
 
   // ******** Hook delegates ********
 
+  /**
+   * @see https://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+   */
   public function onInstall() {
     $files = glob($this->extensionDir . '/sql/*_install.sql');
     if (is_array($files)) {
@@ -300,4 +303,5 @@ class CRM_Mosaico_Upgrader_Base {
       default:
     }
   }
+
 }

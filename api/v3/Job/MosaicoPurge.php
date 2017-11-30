a769b8d15dd44eaa3e355c26778bf0f0d4099dae
Tim Otten <totten@civicrm.org>
2017-11-29 21:15:46 -0800

    Implement APIs to migrate Mosaico 1.x=>2.x

---
+++ api/v3/Job/MosaicoPurge.php
@@ -0,0 +1,27 @@
+<?php
+use CRM_Mosaico_ExtensionUtil as E;
+
+/**
+ * Job.mosaico_purge API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_job_mosaico_purge_spec(&$spec) {
+}
+
+/**
+ * Job.mosaico_purge API
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_job_mosaico_purge($params) {
+  CRM_Core_DAO::executeQuery('DELETE FROM civicrm_mosaico_msg_template');
+  return civicrm_api3_create_success(array(), $params, 'Job', 'mosaico_purge');
+}
---
+++ tests/phpunit/api/v3/Job/MosaicoMigrateTest.php
@@ -0,0 +1,87 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+/**
+ * Job.MosaicoMigrate API Test Case
+ * This is a generic test class implemented with PHPUnit.
+ * @group headless
+ */
+class api_v3_Job_MosaicoMigrateTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  /**
+   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+   * See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+   */
+  public function setUpHeadless() {
+    return \Civi\Test::headless()
+      ->installMe(__DIR__)
+      ->apply();
+  }
+
+  /**
+   * The setup() method is executed before the test is executed (optional).
+   */
+  public function setUp() {
+    parent::setUp();
+  }
+
+  /**
+   * The tearDown() method is executed after the test was executed (optional)
+   * This can be used for cleanup.
+   */
+  public function tearDown() {
+    parent::tearDown();
+  }
+
+  /**
+   * Simple example test case.
+   *
+   * Note how the function name begins with the word "test".
+   */
+  public function testMigrate() {
+    $this->createExampleLegacyTemplate();
+    $this->assertEquals(1, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_msg_template'));
+    $this->assertEquals(0, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_template'));
+
+    $result = civicrm_api3('Job', 'mosaico_migrate', array());
+    $this->assertEquals(1, count($result['values']));
+    foreach ($result['values'] as $k => $v) {
+      $this->assertEquals($k, $v['id']);
+      $this->assertEquals('versafix-1', $v['base']);
+    }
+    $this->assertEquals(1, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_msg_template'));
+    $this->assertEquals(1, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_template'));
+
+    $tpl = civicrm_api3('MosaicoTemplate', 'getsingle', array());
+    $this->assertEquals('The Name', $tpl['title']);
+    $this->assertEquals('versafix-1', $tpl['base']);
+
+    $result = civicrm_api3('Job', 'mosaico_purge', array());
+    $this->assertEquals(0, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_msg_template'));
+    $this->assertEquals(1, CRM_Core_DAO::singleValueQuery('SELECT count(*) FROM civicrm_mosaico_template'));
+  }
+
+  protected function createExampleLegacyTemplate() {
+    $msgTpl = civicrm_api3('MessageTemplate', 'create', array(
+      'msg_title' => 'The Title',
+      'msg_subject' => 'The Subject',
+      'msg_html' => '<p>Placeholder</p>',
+    ));
+
+    CRM_Core_DAO::executeQuery('
+      INSERT INTO civicrm_mosaico_msg_template (msg_tpl_id, hash_key, name, html, metadata, template)
+      VALUES (%1, "1234abcd", "The Name", "<p>The markup</p>", %2, %3)
+    ', array(
+      1 => array($msgTpl['id'], 'Positive'),
+      2 => array(
+        '{"template":"http://dcase.l/sites/all/modules/civicrm/ext/mosaico/packages/mosaico/templates/versafix-1/template-versafix-1.html","name":"No name","created":1512016950525,"editorversion":"0.15.0","templateversion":"1.0.5","changed":1512016954547}',
+        'String',
+      ),
+      3 => array(json_encode(array('type' => 'template')), 'String'),
+    ));
+  }
+
+}

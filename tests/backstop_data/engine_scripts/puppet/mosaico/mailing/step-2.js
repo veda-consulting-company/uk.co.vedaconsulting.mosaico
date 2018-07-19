1ac4fbbb29f44df1958fe22cfc957c9cdb0bffa8
Swastik Pareek <swastik1608@gmail.com>
2018-07-19 18:52:54 +0530

    SHOR-4: PR feedback - Remove comments and fix code

--- tests/backstop_data/engine_scripts/puppet/mosaico/mailing/step-2.js
+++ tests/backstop_data/engine_scripts/puppet/mosaico/mailing/step-2.js
@@ -2,6 +2,5 @@
 
 module.exports = async (engine, scenario, viewport) => {
   await require('./step-1.js') (engine, scenario, viewport);
-  // Wait for Angular to attach events
   await engine.click('.crm_wizard .nav > li:nth-child(2) > a.ng-binding');
 }
--- tests/backstop_data/engine_scripts/puppet/mosaico/mailing/step-3.js
+++ tests/backstop_data/engine_scripts/puppet/mosaico/mailing/step-3.js
@@ -2,9 +2,6 @@
 
 module.exports = async (engine, scenario, viewport) => {
   await require('./step-1.js') (engine, scenario, viewport);
-  // Wait for Angular to attach events
-  // await engine.waitFor(200);
-  // await engine.waitFor('.crm_wizard .nav > li:nth-child(3) > a');
   await engine.click('.crm_wizard .nav > li:nth-child(3) > a.ng-binding');
-  // await engine.click('#schedule-send-at');
+  await engine.click('#schedule-send-at');
 }

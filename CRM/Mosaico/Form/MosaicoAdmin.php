f31c12820c82f085e4ba1f5311bd029162b4a83a
Deepak Srivastava <deepak.srivastava.0303@gmail.com>
2018-12-13 11:26:59 +0000

    Revert "Update Mosaico admin form for 5.8"

---
+++ CRM/Mosaico/Form/MosaicoAdmin.php
@@ -0,0 +1,25 @@
+<?php
+
+use CRM_Mosaico_ExtensionUtil as E;
+
+/**
+ * Form controller class
+ *
+ * @see https://wiki.civicrm.org/confluence/display/CRMDOC/QuickForm+Reference
+ */
+class CRM_Mosaico_Form_MosaicoAdmin extends CRM_Admin_Form_Setting {
+
+  protected $_settings = array(
+    'mosaico_layout' => 'Mosaico Preferences',
+    'mosaico_custom_templates_dir' => 'Mosaico Custom Templates Directory',
+    'mosaico_custom_templates_url' => 'Mosaico Custom Templates URL'
+  );
+
+  /**
+   * Build the form object.
+   */
+  public function buildQuickForm() {
+    parent::buildQuickForm();
+  }
+
+}

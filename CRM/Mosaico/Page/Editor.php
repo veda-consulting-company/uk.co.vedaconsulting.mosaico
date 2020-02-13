c4de2f01e3c7b0c26dbdc849ec632153b610da0c
Matthew Wire <mjw@mjwconsult.co.uk>
2020-02-13 19:18:05 +0000

    Fix path to translation files

--- CRM/Mosaico/Page/Editor.php
+++ CRM/Mosaico/Page/Editor.php
@@ -105,7 +105,7 @@ class CRM_Mosaico_Page_Editor extends CRM_Core_Page {
     // Adding translation strings if exist
     $locale = CRM_Core_I18n::getLocale();
     $lang = CRM_Core_I18n_PseudoConstant::shortForLong($locale);
-    $translationFile = CRM_Core_Resources::singleton()->getPath(E::LONG_NAME, "packages/mosaico/res/lang/mosaico-{$lang}.json");
+    $translationFile = CRM_Core_Resources::singleton()->getPath(E::LONG_NAME, "packages/mosaico/dist/lang/mosaico-{$lang}.json");
     if (file_exists($translationFile)) {
       $config['strings'] = json_decode(file_get_contents($translationFile));
     }

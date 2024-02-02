9a8f5c33ea54506a791163e10bcbb0741f9a4cd2
Tim Otten <totten@civicrm.org>
2024-02-01 17:29:51 -0800

    Update main layout to allow A/B testing for the "Design"

--- ang/crmMosaico/MixinCtrl.js
+++ ang/crmMosaico/MixinCtrl.js
@@ -2,8 +2,29 @@
 
   // This provides additional actions for editing a Mosaico mailing.
   // It coexists with crmMailing's EditMailingCtrl.
-  angular.module('crmMosaico').controller('CrmMosaicoMixinCtrl', function CrmMosaicoMixinCtrl($scope, dialogService) {
-    // var ts = $scope.ts = CRM.ts(null);
+  angular.module('crmMosaico').controller('CrmMosaicoMixinCtrl', function CrmMosaicoMixinCtrl($scope, dialogService, crmMosaicoVariants) {
+    const ts = $scope.ts = CRM.ts('mosaico');
+
+    const singleDesign = [
+      {title: ts('Design'), vid: null, action: 'split'}
+    ];
+    const abDesign = [
+      {title: ts('Design (A)'), vid: 0, action: 'unsplit'},
+      {title: ts('Design (B)'), vid: 1, action: 'unsplit'},
+    ];
+
+    $scope.getDesigns = function(mailing) {
+      if (!mailing) return [];
+      return crmMosaicoVariants.isSplit(mailing, 'body_html') ? abDesign : singleDesign;
+    }
+    $scope.unsplitDesign = function(mailing, vid) {
+      crmMosaicoVariants.remove(mailing, ['mosaicoTemplate', 'mosaicoMetadata', 'mosaicoContent', 'body_html'], vid);
+    };
+    $scope.splitDesign = function(mailing) {
+      crmMosaicoVariants.split(mailing, ['mosaicoTemplate', 'mosaicoMetadata', 'mosaicoContent', 'body_html']);
+    };
+
+    $scope.isMailingSplit = (mailing, field) => crmMosaicoVariants.isSplit(mailing, field);
 
     var activeDialogs = {};
 

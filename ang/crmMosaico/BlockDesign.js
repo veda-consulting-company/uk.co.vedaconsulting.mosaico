461831562ed49f4185c350838687f9015a290e02
Tim Otten <totten@civicrm.org>
2024-02-01 16:44:09 -0800

    crmMosaicoBlockDesign - Explicitly pass-through scope.attachments

--- ang/crmMosaico/BlockDesign.js
+++ ang/crmMosaico/BlockDesign.js
@@ -3,12 +3,16 @@
     return {
       scope: {
         crmMosaicoCtrl: '@',
-        crmMailing: '@'
+        crmMailing: '@',
+        crmMailingAttachments: '@'
       },
       templateUrl: '~/crmMosaico/BlockDesign.html',
       link: function (scope, elm, attr) {
         scope.$parent.$watch(attr.crmMailing, function(newValue){
           scope.mailing = newValue;
+        });
+        scope.$parent.$watch(attr.crmMailingAttachments, function(newValue){
+          scope.attachments = newValue;
         });
         scope.$parent.$watch(attr.crmMosaicoCtrl, function(newValue){
           scope.mosaicoCtrl = newValue;
